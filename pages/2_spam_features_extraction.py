6cb51399a81201d0effec0fcad3261826e431f26
yusuf26 <hamdadiyusuf@gmail.com>
2022-07-07 14:24:36 +0700

    Create 2_spam_features_extraction.py

---
+++ pages/2_spam_features_extraction.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+from urllib.error import URLError
+
+st.set_page_config(page_title="Spam Features Extraction")
+
+st.markdown("# Spam Features Extraction")
+st.sidebar.header("Spam Features Extraction")
+st.write(
+    "Spam Features Extraction"
+)
