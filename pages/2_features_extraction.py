617df4020ebdbb6da4ee5f52029bc68946de7ba4
yusuf26 <hamdadiyusuf@gmail.com>
2022-07-07 14:21:05 +0700

    Create 2_features_extraction.py

---
+++ pages/2_features_extraction.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+from urllib.error import URLError
+
+st.set_page_config(page_title="Features Extration")
+
+st.markdown("# Features Extration with TF-IDF")
+st.sidebar.header("Features Extration with TF-IDF")
+st.write(
+    """Features Extration with TF-IDF"""
+)
