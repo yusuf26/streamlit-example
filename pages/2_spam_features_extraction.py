4f8d7902838e907c70ad1970c7748d4bad9d8c14
yusuf26 <hamdadiyusuf@gmail.com>
2022-07-07 14:26:01 +0700

    Update 2_spam_features_extraction.py

--- pages/2_spam_features_extraction.py
+++ pages/2_spam_features_extraction.py
@@ -5,7 +5,7 @@ from urllib.error import URLError
 
 st.set_page_config(page_title="Spam Features Extraction")
 
-st.markdown("# Spam Features Extraction")
+st.markdown("# Spam Features Extraction with TF-IDF")
 st.sidebar.header("Spam Features Extraction")
 st.write(
     "Spam Features Extraction"
