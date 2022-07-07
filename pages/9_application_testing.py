4cfbb50d0f10482bc24e0e54afbaf4fb07299765
yusuf26 <hamdadiyusuf@gmail.com>
2022-07-07 14:40:00 +0700

    Create 9_application_testing.py

---
+++ pages/9_application_testing.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+from urllib.error import URLError
+
+st.set_page_config(page_title="Application Testing")
+
+st.markdown("# Application Testing")
+st.sidebar.header("Application Testing")
+st.write(
+    "Application Testing"
+)
