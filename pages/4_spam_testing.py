ff948df9725d972ac4f545991879e55c3dbb21aa
yusuf26 <hamdadiyusuf@gmail.com>
2022-07-07 14:28:46 +0700

    Create 4_spam_testing.py

---
+++ pages/4_spam_testing.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+from urllib.error import URLError
+
+st.set_page_config(page_title="Spam Testing")
+
+st.markdown("# Spam Testing")
+st.sidebar.header("Spam Testing")
+st.write(
+    "Spam Testing"
+)
