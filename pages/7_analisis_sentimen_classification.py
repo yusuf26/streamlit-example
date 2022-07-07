a9e7872c6cde4f5f16f8e523d38871fc611a3df7
yusuf26 <hamdadiyusuf@gmail.com>
2022-07-07 14:36:33 +0700

    Create 7_analisis_sentimen_classification.py

---
+++ pages/7_analisis_sentimen_classification.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+from urllib.error import URLError
+
+st.set_page_config(page_title="Analisis Sentimen Classification")
+
+st.markdown("# Analisis Sentimen Classification with Multinominal Naive Bayes and Random Forest")
+st.sidebar.header("Analisis Sentimen Classification with Multinominal Naive Bayes and Random Forest")
+st.write(
+    "Analisis Sentimen Classification with Multinominal Naive Bayes and Random Forest"
+)
