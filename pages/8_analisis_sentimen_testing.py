11026311048462d5fc14ed994d6b361b47709c09
yusuf26 <hamdadiyusuf@gmail.com>
2022-07-07 14:39:05 +0700

    Create 8_analisis_sentimen_testing.py

---
+++ pages/8_analisis_sentimen_testing.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+from urllib.error import URLError
+
+st.set_page_config(page_title="Analisis Sentimen Testing")
+
+st.markdown("# Analisis Sentimen Testing")
+st.sidebar.header("Analisis Sentimen Testing")
+st.write(
+    "Analisis Sentimen Testing"
+)
