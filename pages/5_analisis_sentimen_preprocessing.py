6dabd77b1b37c99086967492c0b84237483314e7
yusuf26 <hamdadiyusuf@gmail.com>
2022-07-07 14:30:17 +0700

    Create 5_analisis_sentimen_preprocessing.py

---
+++ pages/5_analisis_sentimen_preprocessing.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+from urllib.error import URLError
+
+st.set_page_config(page_title="Analisis Sentimen Preprocessing")
+
+st.markdown("# Analisis Sentimen Preprocessing")
+st.sidebar.header("Analisis Sentimen Preprocessing")
+st.write(
+    "Analisis Sentimen Preprocessing"
+)
